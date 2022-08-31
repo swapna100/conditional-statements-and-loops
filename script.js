0a50686b21e4eed74c13e5fef796b848f69004c0
swapna samuel <33215403+swapna100@users.noreply.github.com>
2022-08-31 12:40:22 +0200

    update

--- script.js
+++ script.js
@@ -2,24 +2,28 @@
 
 function Largest2No() {
     var n1,n2;
-    n1 = parseInt(prompt("enter first number:"));
-    n2 = parseInt(prompt("enter second number:"));
+    n1 = 15;
+    n2 = 5;
   if(n1>n2)
   {
-     document.write(n1+" is Largest number");
+     
+     console.log(n1+" is Larger than "+ n2);
   }
   else
   {
-     document.write(n2+" is Largest number");
+     
+     console.log(n2+" is Larger than " + n1);
   }
  }
+Largest2No();
 
  //Program to find the number is even or odd
  // take input from the user
  function EvenorOdd(){
-const number = prompt("Enter a number: ");
+const number = 36;
 
 // ternary operator
 const result = (number % 2  == 0) ? "even" : "odd";
-document.write( "The number is "+ result);
+console.log(`The number ${number} is `+ result);
  }
+ EvenorOdd();
